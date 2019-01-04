8a27a2e62923f96215737f28c036991ae86af9ff
Surfjenser <surfjenser@googlemail.com>
2019-01-04 10:42:42 +0100

    added a simple CMake project

---
+++ CMakeProject_XTR/CMakeProject_XTR.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject_XTR.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeProject_XTR.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << endl << "Press Enter to exit...";
+	cin.get();
+	return 0;
+}
